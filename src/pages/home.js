330b5b916e96b4077490e0c206ca9f09a7947350
Umair Khalid <umair@zaviago.com>
2023-09-20 10:47:11 +0500

    meditime

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
+import kImaging from "../img/4kImaging.png";
+import kImagingHover from "../img/4kImagingHover.png";
+import shoulderRestortion from "../img/shoulderRestortion.png";
+import shoulderRestortionHover from "../img/shoulderRestortionHover.png";
+import kneePreservation from "../img/kneePreservation.png";
+import kneePreservationHover from "../img/kneePreservationHover.png";
+import extremitySecurity from "../img/extremitySecurity.png";
+import extremitySecurityHover from "../img/extremitySecurityHover.png";
+import poweredInstruction from "../img/poweredInstruction.png";
+import poweredInstructionHover from "../img/poweredInstructionHover.png";
 
 import HomeSlider from "../components/homeslider";
+
+const upperBox = [
+  {
+    title:'Sport Medicine',
+    desc:'4K Imaging System, Shoulder Restoration System, Knee Repair and Construction System และ Extremity System',
+    btnText:'ดูเพิ่มเติม',
+    btnLink: 'https://www.conmed.com/en-US/Specialties-and-Techniques/Specialties/Orthopedics',
+  },
+  {
+    title:'Powered Instruments',
+    desc:'Large bone, small bone, sternal saw, high-speed drill',
+    btnText:'ดูเพิ่มเติม',
+    btnLink: 'https://www.conmed.com/en-US/Products/Surgical-Power-Tools',
+  },
+  {
+    title:'VWELL Products',
+    desc:'Personalized Insole',
+    btnText:'ดูเพิ่มเติม',
+    btnLink: 'https://www.vwell.care/footcare',
+  },
+]
+
+const imageItems = [
+  {
+    key: "kImaging",
+    image: kImaging,
+    hoverImage: kImagingHover,
+  },
+  {
+    key: "shoulderRestortion",
+    image: shoulderRestortion,
+    hoverImage: shoulderRestortionHover,
+  },
+  {
+    key: "kneePreservation",
+    image: kneePreservation,
+    hoverImage: kneePreservationHover,
+  },
+  {
+    key: "extremitySecurity",
+    image: extremitySecurity,
+    hoverImage: extremitySecurityHover,
+  },
+  {
+    key: "poweredInstruction",
+    image: poweredInstruction,
+    hoverImage: poweredInstructionHover,
+  },
+];
+
 const Home = () => {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (key) => {
+    setHoveredItem(key);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  const renderImageContainers = () => {
+    return imageItems.map((item) => (
+      <div
+        key={item.key}
+        className="image-container w-1/5 h-[230px] leading-4 bg-no-repeat bg-contain"
+        onMouseEnter={() => handleMouseEnter(item.key)}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          backgroundImage: `url(${hoveredItem === item.key ? item.hoverImage : item.image})`,
+        }}
+      ></div>
+    ));
+  };
+
   return (
     <>
-    <section className="h-[280px]">
-    <HomeSlider />
+      <section className="h-[280px]">
+        <HomeSlider />
+      </section>
+      <main>
+        <section className=" bg-[#E5F0EB]">
+        <div className="max-w-[1200px] mx-auto flex justify-around gap-x-14 py-24">
 
-    </section>
-    <main>
+       
+      {upperBox.map((item, index) => (
+        <div key={index} className="bg-[#006B3D] w-1/3 p-8 text-white rounded-[10px] flex flex-col gap-5">
+          <h2 className="text-[24px] font-bold">{item.title}</h2>
+          <p className="text-base font-medium">{item.desc}</p>
+          <a href={item.btnLink} target="_blank" rel="noopener noreferrer">
+            {item.btnText}
+          </a>
+        </div>
+      ))}
+    
+          
 
-    </main>
+          </div>
+        </section>
+        <section className="max-w-[1270px] mx-auto interesting-products mt-28">
+          <h2 className="text-[#003190] text-[48px] font-bold text-center">สินค้าที่น่าสนใจ</h2>
+          <div className="flex gap-x-5 mt-28">{renderImageContainers()}</div>
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
 export default Home;
