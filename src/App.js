b06d01e84e6070124cf976b2220f26dd953213f7
Muhammad Umair Khalid <135826725+mianumairkhalid@users.noreply.github.com>
2023-09-22 11:12:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React from 'react'; // Make sure to import React
+import React from 'react'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import ContactUs from './pages/contact-us';
-import AboutUs from './pages/about-us';
-import AfterSaleService from './pages/after-sale-service';
-import NewCategories from './pages/new-categories';
-import BlogDetail from './pages/blog-detail';
-import Header from './components/header';
-import Footer from './components/footer';
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         {/* Define your routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/บริการหลังการขาย" element={<AfterSaleService />} />
-        <Route path="/new-categories" element={<NewCategories />} />
-        <Route path="/blog-detail" element={<BlogDetail />} />
-
-
-        {/* Add a catch-all route or a 404 page */}
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
