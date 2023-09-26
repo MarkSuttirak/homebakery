c9a1706f43cce9572c0c93e959fa526420ff9bce
Umair Khalid <umair@zaviago.com>
2023-09-26 15:43:25 +0500

    our story

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ourStoryBgImg from "../img/ourStoryBgImg.png";
+
 import Delicious from "../img/Delicious.png";
 import Healthy from "../img/Healthy.png";
 import Homemade from "../img/Homemade.png";
@@ -27,8 +29,24 @@ const incentives = [
 function OurStory() {
   return (
     <main>
-        <section className="bg-white">
-        <div className="mx-auto max-w-7xl py-24 sm:px-2 sm:py-32 lg:px-4">
+        <section
+          className="min-h-[50vh] relative flex items-center coverCenter"
+          style={{ background: `url('${ourStoryBgImg}')` }}
+        >
+          {/* <div className="background-overlay"></div> */}
+          <div className="container mx-auto max-w-[700px] px-6 flex flex-col justify-center text-center min-h-[600px]">
+            <h1 className="text-white text-shadow-lg font-Avenir text-[81px] font-bold leading-[68px] tracking-[2.43px] uppercase relative">
+            Our story
+            </h1>
+            
+            <p className="text-white text-shadow-sm font-noto-sans-thai text-2xl font-normal leading-[28px] mt-5">
+            เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ 
+จำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่
+            </p>
+          </div>
+        </section>
+        <section className="bg-white mt-[120px]">
+        <div className="mx-auto max-w-7xl sm:px-2 lg:px-4">
           <div className="mx-auto max-w-2xl px-4 lg:max-w-none">
             <div className="max-w-3xl mx-auto text-center">
               <h2 className="text-[#795E32] font-Avenir text-[48px] font-bold leading-[68px] tracking-[1.44px]">
