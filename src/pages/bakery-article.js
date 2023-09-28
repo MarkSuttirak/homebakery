c989f1270683dbc22619acd94904dd4f42191038
Umair Khalid <umair@zaviago.com>
2023-09-28 10:03:16 +0500

    bakery article

--- src/pages/bakery-article.js
+++ src/pages/bakery-article.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import bakeryArticleBgImg from "../img/bakeryArticleBgImg.png";
+import featuredBlogImg from "../img/featuredBlogImg.png";
 
 function BakeryArticle() {
+  const activeTabClass = "bg-[#F2E7D6] text-[#795E32]";
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabIndex) => {
+    setActiveTab(tabIndex);
+  };
+
   return (
     <main>
       <section
@@ -18,6 +26,70 @@ function BakeryArticle() {
             เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์
             จำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่
           </p>
+        </div>
+      </section>
+      <section className="mt-[120px]">
+        <div className="max-w-[1300px] mx-auto flex">
+          <div>
+            <img src={featuredBlogImg} alt="" />
+          </div>
+          <div className="pl-[130px]">
+            <h2 className="text-[#795E32] text-[40px] font-bold font-Avenir">
+              The Secret of Toffee Cake
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros. Consectetur adipiscing elit.
+            </p>
+            <button className="w-[177px] h-[47px] bg-[#E41E25] uppercase text-white rounded-full mt-[40px] ">
+              VIEW MORE
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="mt-[130px]">
+        <div className="max-w-[1300px] mx-auto">
+          <div className="flex gap-x-6">
+            <button
+              onClick={() => handleTabClick(1)}
+              className={`px-4 py-2 text-[#795E32] ${
+                activeTab === 1 ? "bg-[#F2E7D6] text-[#795E32]" : "bg-[#F8F3EB]"
+              } rounded`}
+            >
+              View All
+            </button>
+            <button
+              onClick={() => handleTabClick(2)}
+              className={`px-4 py-2 text-[#795E32] ${
+                activeTab === 2 ? "bg-[#F2E7D6] text-[#795E32]" : "bg-[#F8F3EB]"
+              } rounded`}
+            >
+              Toffee Cake
+            </button>
+            <button
+              onClick={() => handleTabClick(3)}
+              className={`px-4 py-2 text-[#795E32] ${
+                activeTab === 3 ? "bg-[#F2E7D6] text-[#795E32]" : "bg-[#F8F3EB]"
+              } rounded`}
+            >
+              Bakery
+            </button>
+            <button
+              onClick={() => handleTabClick(4)}
+              className={`px-4 py-2 text-[#795E32] ${
+                activeTab === 4 ? "bg-[#F2E7D6] text-[#795E32]" : "bg-[#F8F3EB]"
+              } rounded`}
+            >
+              Sweet
+            </button>
+          </div>
+
+          <div className="mt-4">
+            {activeTab === 1 && <div>Tab 1 content goes here</div>}
+            {activeTab === 2 && <div>Tab 2 content goes here</div>}
+            {activeTab === 3 && <div>Tab 3 content goes here</div>}
+            {activeTab === 4 && <div>Tab 4 content goes here</div>}
+          </div>
         </div>
       </section>
     </main>
